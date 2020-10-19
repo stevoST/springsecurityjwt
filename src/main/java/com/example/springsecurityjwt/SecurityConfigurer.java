dfa0fc0e581f7a12f832e1a19b4173a2ac156cf6
Starosta Štefan (ERNI) <stefan.starosta@betterask.erni>
2020-10-19 15:32:17 +0200

    fist commit

---
+++ src/main/java/com/example/springsecurityjwt/SecurityConfigurer.java
@@ -0,0 +1,27 @@
+package com.example.springsecurityjwt;
+
+import com.example.springsecurityjwt.Services.MyuserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyuserDetailsService myuserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myuserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
