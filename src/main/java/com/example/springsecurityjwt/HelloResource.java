6029bfe02f3c95555245d171ba2b58a1f5d248db
Starosta Štefan (ERNI) <stefan.starosta@betterask.erni>
2020-10-21 17:32:12 +0200

    working on authenticate endpoint

--- src/main/java/com/example/springsecurityjwt/HelloResource.java
+++ src/main/java/com/example/springsecurityjwt/HelloResource.java
@@ -1,14 +1,42 @@
 package com.example.springsecurityjwt;
 
-import org.springframework.stereotype.Controller;
+import com.example.springsecurityjwt.Services.MyuserDetailsService;
+import com.example.springsecurityjwt.models.AuthenticationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private MyuserDetailsService myuserDetailsService;
+
     @RequestMapping("/hello")
     public String hello(){
         return "Hello world";
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public RequestEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = myuserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
     }
 }
